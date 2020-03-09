d3695fbf7472ec1a658edff6e8d631afc3963a04
Ivan <fivanunam@gmail.com>
2020-03-09 11:53:12 -0600

    fix user test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -83,12 +83,4 @@ class UserTest < ActiveSupport::TestCase
       @user.destroy
     end
   end
-
-  test "associated microposts should be destroyed" do
-    @user.save
-    @user.microposts.create!(content: "Lorem ipsum")
-    assert_difference 'Micropost.count', -1 do
-      @user.destroy
-    end
-  end
 end
